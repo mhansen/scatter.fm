bc1406abb7e829dbb7ec7bc848e47050889b38e4
Mark Hansen <mark@markhansen.co.nz>
2025-01-02 11:42:36 +1100

    revert usage of Map
    underscore checks isEqual, but doesn't support equality on Map objects https://underscorejs.org/docs/modules/isEqual.html
    
    In a previous commit i replaced a raw object with a Map
    
    So the initial request ends up triggering a change on the legend, but later requests don't, because they compare equal incorrectly.
    
    maybe this is fixed in new backbone/underscore, idk.

--- public/views/LegendView.js
+++ public/views/LegendView.js
@@ -1,8 +1,9 @@
 class LegendView extends Backbone.View {
     render() {
         this.$("li").remove();
-        let artistColors = legendModel.get("artistColors");
-        for (const color of artistColors.values()) {
+        let artistColors = legendModel.artist_colors();
+        for (const k in artistColors) {
+            const color = artistColors[k];
             if (color.showInLegend) {
                 $("<li>")
                     .text(`${color.artist} (${color.count})`)
